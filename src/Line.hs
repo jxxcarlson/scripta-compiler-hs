2456589e9ae167a5cd968c37559499a62c4b2b3d
James Carlson <jxxcarlson@gmail.com>
2022-08-31 14:39:25 -0400

    Continuing ...

--- src/Line.hs
+++ src/Line.hs
@@ -1,16 +1,57 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DuplicateRecordFields #-}
 
-module Line(PrimitiveBlockType(..), Line, classify) where
+module Line(PrimitiveBlockType(..), Line, indent, prefix, content, lineNumber, position, classify, getBlockType) where
 
-import Data.Text.Lazy ( unpack, Text )
+import Data.Text.Lazy as Text ( unpack, Text, take, strip )
 import Data.Text.Lazy.Encoding 
+import Language (Language(..)) 
 
 data PrimitiveBlockType = PBVerbatim | PBOrdinary | PBParagraph deriving (Show)
 
 data Line =
-    Line { indentL :: Int, prefixL :: Text, contentL :: Text, lineNumberL :: Int, positionL :: Int }
+    Line { indent :: Int, prefix :: Text, content :: Text, lineNumber :: Int, position :: Int }
 
 
 classify :: Int -> Int -> String -> Line
 classify position lineNumber str =
-    Line {indentL = 0, prefixL = "", contentL = "", lineNumberL = 0, positionL = 0}
+    Line {indent = 0, prefix = "", content = "", lineNumber = 0, position = 0}
+
+getBlockType :: Language -> Text -> PrimitiveBlockType
+getBlockType lang line_ =
+    let
+        line =
+            Text.strip line_
+    in
+    case lang of
+        L0Lang ->
+            if Text.take 2 line == "||" then
+                PBVerbatim
+
+            else if Text.take 2 line == "$$" then
+                PBVerbatim
+
+            else if
+                Text.take 1 line
+                    == "|"
+            then
+                PBOrdinary
+
+            else
+                PBParagraph
+
+        MicroLaTeXLang ->
+            -- Note the source text has already been partially transformed to conform to L0
+            if Text.take 2 line == "||" then
+                PBVerbatim
+
+            else if Text.take 2 line == "$$" then
+                PBVerbatim
+
+            else if
+                Text.take 1 line
+                    == "|"
+            then
+                PBOrdinary
+
+            else
+                PBParagraph
--- src/PrimitiveBlock.hs
+++ src/PrimitiveBlock.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DuplicateRecordFields, OverloadedLabels #-}
 
+-- OverloadedRecordDot
+-- https://pure-hack.com/posts/overloaded-labels-in-haskell/
 
 module PrimitiveBlock (PrimitiveBlock, empty, Step(..), loop) where
 
-import Data.Text.Lazy ( unpack, Text )
-import Data.Text.Lazy.Encoding 
-import Line (PrimitiveBlockType,Line,classify)   
+import Data.Text.Lazy as Text (Text, intercalate, empty, length, fromChunks, strip)
+import Line (PrimitiveBlockType(..),Line,indent, prefix, content, lineNumber, position, classify, getBlockType) 
+import Language (Language(..)) 
+import Flow ((|>))
 
 data PrimitiveBlock = PrimitiveBlock
     { indent :: Int
@@ -39,18 +42,17 @@ data State =
    State { blocks ::  [PrimitiveBlock]
     , currentBlock :: Maybe PrimitiveBlock
     , lang :: Language
-    , linesS :: [Text]
+    , lines_ :: [Text]
     , inBlock :: Bool
-    , indentS :: Int
-    , lineNumberS :: Int
-    , positionS :: Int
+    , indent :: Int
+    , currentLineNumber :: Int
+    , cursor :: Int
     , inVerbatim :: Bool
     , isVerbatimLine :: Text -> Bool
     , count :: Int
     , label :: Text
     }
 
-data Language = L0Lang | MicroLaTeXLang deriving  (Show)
 
 data Step state a
     = Loop state
@@ -63,19 +65,19 @@ language and a function for determining when a string is the first line
 of a verbatim block
 -}
 parse ::Language -> (Text -> Bool) ->  [Text] ->  [PrimitiveBlock]
-parse lang isVerbatimLine lines =
+parse lang isVerbatimLine lines_ =
     case lang of
         L0Lang ->
-            lines |> parse_ lang isVerbatimLine
+            lines_ |> parse_ lang isVerbatimLine
 
         MicroLaTeXLang ->
-            --lines |> MicroLaTeX.Parser.TransformLaTeX.toL0 |> parse_ lang isVerbatimLine
-            lines |> parse_ lang isVerbatimLine
+            --lines_ |> MicroLaTeX.Parser.TransformLaTeX.toL0 |> parse_ lang isVerbatimLine
+            lines_ |> parse_ lang isVerbatimLine
 
         
 parse_ :: Language -> (String -> Bool) ->  [Text] -> [PrimitiveBlock]
-parse_ lang isVerbatimLine lines =
-    loop (init lang isVerbatimLine lines) nextStep
+parse_ lang isVerbatimLine lines_ =
+    loop (init lang isVerbatimLine lines_) nextStep
         |> map (\block -> finalize block)
 
 head_ :: [a] -> Maybe a 
@@ -84,7 +86,7 @@ head_ (first:srest) = Just first
 
 nextStep :: State -> Step State  [PrimitiveBlock]
 nextStep state =
-    case head_ $ linesS $ state of
+    case head_ $ lines_ $ state of
         Nothing ->
             case currentBlock state of
                 Nothing ->
@@ -92,39 +94,39 @@ nextStep state =
 
                 Just block ->
                     let
-                        blocks =
-                            if content block == [ "" ] then
+                        newBlocks =
+                            if PrimitiveBlock.content block == [ "" ] then
                                 -- Debug.log (Tools.cyan "****, DONE" 13)
                                 reverse (blocks state)
 
                             else
                                 -- Debug.log (Tools.cyan "****, DONE" 13)
-                                reverse (block :: blocks state)
+                                reverse (block : (blocks state))
                     in
-                    Done blocks
+                    Done newBlocks
 
         Just rawLine ->
             let
-                newPosition =
+                newCursor =
                     if rawLine == "" then
-                        position state + 1
+                        cursor state + 1
 
                     else
-                        position state + length rawLine + 1
+                        cursor state + (Text.length rawLine |> fromIntegral) + 1
 
                 currentLine :: Line
                 currentLine =
                     -- TODO: the below is wrong
-                    Line.classify (position state) (lineNumber state + 1) rawLine
+                    Line.classify (cursor state) (currentLineNumber state + 1) rawLine
             in
-            case ( inBlock state, isEmpty currentLine, isNonEmptyBlank currentLine ) of
+            case ( inBlock state, currentLine == Text.empty, isNonEmptyBlank currentLine ) of
                 -- not in a block, pass over empty line
                 ( False, True, _ ) ->
-                    Loop (advance newPosition state{label = "1, EMPTY" })
+                    Loop (advance newCursor state{label = "1, EMPTY" })
 
                 -- not in a block, pass over blank, non-empty line
                 ( False, False, True ) ->
-                    Loop (advance newPosition state{label = "2, PASS" })
+                    Loop (advance newCursor state{label = "2, PASS" })
 
                 -- create a new block: we are not in a block, but
                 -- the current line is nonempty and nonblank
@@ -148,3 +150,101 @@ loop s f =
         Done b -> b
 
 
+-- FUNCTIONS
+
+finalize :: PrimitiveBlock -> PrimitiveBlock
+finalize block =
+    let
+        finalContent =
+           reverse (PrimitiveBlock.content block)
+
+        sourceText =
+            -- String.join "\n" content
+            Text.intercalate "\n" finalContent
+
+    in
+    PrimitiveBlock{content = finalContent, sourceText = sourceText }
+
+
+isNonEmptyBlank :: Line -> Bool
+isNonEmptyBlank line =
+   Line.indent line > 0 && Line.content line == Text.empty
+
+
+advance :: Int -> State -> State
+advance newCursor state =
+    state{lines_ = drop 1 (lines_ state)
+        , currentLineNumber = (currentLineNumber state) + 1
+        , cursor = newCursor
+        , count = (count state) + 1
+    }
+
+createBlock :: State -> Line -> State
+createBlock state currentLine =
+    let
+        newBlocks =
+            case currentBlock state of
+                Nothing ->
+                    blocks state
+
+                -- When creating a new block push the current block onto state.blocks
+                -- only if its content is nontrivial (not == [""])
+                Just block ->
+                    if PrimitiveBlock.content block == [] then
+                        blocks state
+
+                    else
+                        block : (blocks state)
+
+        newBlock =
+            Just (blockFromLine (lang state) currentLine)
+    in
+    state{lines_ = drop 1 (lines_ state)
+        , currentLineNumber = (currentLineNumber state) + 1
+        , cursor = (cursor state) + (Text.length (Line.content currentLine) |> fromIntegral)
+        , count = (count state) + 1
+        , indent = (Line.indent currentLine)
+        , inBlock = True
+        , currentBlock = newBlock
+        , blocks = newBlocks
+    }
+
+blockFromLine :: Language -> Line -> PrimitiveBlock
+-- blockFromLine lang ({ indent, lineNumber, position, prefix, content } as line) =
+blockFromLine lang line =
+   PrimitiveBlock { indent = Line.indent line
+    , lineNumber = Line.lineNumber line
+    , position = Line.position line
+    , content = Text.fromChunks [Line.prefix, Line.content] 
+    , name = Nothing
+    , args = []
+    , named = False
+    , sourceText = ""
+    , blockType = Line.getBlockType lang (Line.content line)
+    }
+        |> elaborate line
+
+
+elaborate :: Line -> PrimitiveBlock -> PrimitiveBlock
+elaborate line pb =
+    if named pb then
+        pb
+
+    else 
+        if (content pb) == [ "" ] then
+        pb
+
+        else
+            let
+                ( name, args ) =
+                    -- TODO: note this change: it needs to be verified
+                    Line.getNameAndArgs L0Lang line
+
+                content =
+                    if (blockType pb) == PBVerbatim then
+                        map Text.strip (content pb)
+
+                    else
+                        (content pb)
+            in
+            pb{ content = content, name = name, args = args, named = True }
